e0477d695e4d76c58309231480b1e6330ab49280
rachupalli <141514256+rachupalli@users.noreply.github.com>
2025-02-25 04:32:52 +0000

    added filter

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import productData from './data/full-products';
 import { Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import CardList from './components/CardList';
+import SingleView from './components/SingleView';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CardList data={productData}/>
+      <Routes>
+          <Route path="/" element={<CardList data={productData} />} />
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+      </Routes>
     </div>
   );
 }
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,6 @@
 import Card from './Card'
 import Button from './Button'
+import Search from './Search'
 import React, { useState, useEffect } from "react";
 
 const CardList = ({data}) => {
@@ -19,17 +20,30 @@ const CardList = ({data}) => {
   useEffect(() => {
     setProducts(data.slice(offset, offset + limit));
   }, [offset, limit, data]);
+
+  const filterTags = (tagQuery) => {
+    const filtered = data.filter(product =>{
+      if (!tagQuery){
+        return product
+      }
+      return product.tags.find(({title})=> title === tagQuery)
+    })
+
+    setOffset(0)
+    setProducts(filtered)
+  }
   
 
   return (
     <div className="cf pa2">
+      <Search handleSearch={filterTags}/>
       <div className="mt2 mb2">
         {products && products.map((product) => 
           ( <Card key={product.id} {...product} />))}
       </div>
       <div className="flex items-center justify-center pa4">   
-        <Button text="Previous" handleClick={handlePrevious} />
-        <Button text="Next" handleClick={handleNext} />
+        <Button text="Previous" handleClick={() => setOffset(offset - limit)} />
+        <Button text="Next" handleClick={() => setOffset(offset + limit)} />
       </div>
     </div>
   )
