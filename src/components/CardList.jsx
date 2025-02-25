d0b18a0d11e103f6c62b63dc49030d91eaede379
rachupalli <141514256+rachupalli@users.noreply.github.com>
2025-02-25 03:41:47 +0000

    Added buttons at the bottom

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,10 +1,16 @@
 import Card from './Card'
+import Button from './Button'
 
 const CardList = ({data}) => {
   return (
     <div className="cf pa2">
       <div className="mt2 mb2">
-        {data && data.map((prodcut) => (<Card key={product.id} {...product} />))}
+        {data && data.map((product) => 
+          ( <Card key={product.id} {...product} />))}
+      </div>
+      <div className="flex items-center justify-center pa4">   
+        <Button text="Previous" />
+        <Button text="Next" />
       </div>
     </div>
   )
