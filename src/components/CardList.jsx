f33991551c6488e98cd73ee236b1d2b17aff7914
rachupalli <141514256+rachupalli@users.noreply.github.com>
2025-02-25 03:11:48 +0000

    corrected cardlist

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,14 +1,10 @@
-import Card from "./Card";
+import Card from './Card'
 
 const CardList = ({data}) => {
   return (
     <div className="cf pa2">
       <div className="mt2 mb2">
-        {data && data.map((prodcut) => {
-          return(
-            <Card key = {products.id} {...product} />
-          )
-        })}
+        {data && data.map((prodcut) => (<Card key={product.id} {...product} />))}
       </div>
     </div>
   )
